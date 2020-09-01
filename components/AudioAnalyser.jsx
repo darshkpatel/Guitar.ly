45d1472a36458331c18e55f0c89e7e5e8e350d99
Darsh <darshkpatel@gmail.com>
2020-09-01 22:28:03 +0530

    analyze notes

--- components/AudioAnalyser.jsx
+++ components/AudioAnalyser.jsx
@@ -1,15 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import AudioVisualiser from './AudioVisualiser';
+import Pitchfinder from "pitchfinder";
+
+const noteStrings = [
+  "C",
+  "C♯",
+  "D",
+  "D♯",
+  "E",
+  "F",
+  "F♯",
+  "G",
+  "G♯",
+  "A",
+  "A♯",
+  "B"
+];
+
+// Simplified from nu.js
+const getNote = freq => {
+  const note = 12 * (Math.log(freq / 440) / Math.log(2));
+  return Math.round(note) + 69;
+};
 
 const AudioAnalyser = ({audio}) => {
   const [audioData, setAudioData] = useState(new Uint8Array(0));
-  let audioContext; let analyser; let dataArray; let source; let
-    rafId;
+  let audioContext;
+  let analyser;
+  let dataArray;
+  let source;
+  let rafId;
+
   const tick = () => {
     analyser.getByteTimeDomainData(dataArray);
     setAudioData({ audioData: dataArray });
+    const detectPitch = new Pitchfinder.AMDF({
+      maxFrequency: 800,
+      minFrequency: 75
+    });
+    const pitch = detectPitch(dataArray);
+    if (pitch) {
+      const freq = Math.floor(pitch);
+      const note = getNote(freq);
+      const noteName = noteStrings[note % 12];
+      const octave = parseInt(note / 12) - 1;
+      console.log({freq,note,noteName,octave});
+    }
     rafId = requestAnimationFrame(tick);
   };
+
   useEffect(() => {
     console.log(audio)
     audioContext = new (window.AudioContext
