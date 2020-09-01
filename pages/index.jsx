e848167882a5617f2e4c77675e1ddd1d4cd61fe4
Darsh <darshkpatel@gmail.com>
2020-09-01 11:15:23 +0530

    added tune page skeleton

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from '../components/homepage.module.css';
 
 const Homepage = () => (
@@ -6,7 +7,9 @@ const Homepage = () => (
       <img src="/assets/bottom_flow_black.svg" alt="bottom flow" />
       <img src="/assets/logo_text.svg" alt="logo and text" className={styles.centerImg} />
     </div>
-    <img src="/assets/get_started.svg" alt="getting started button" className={styles.getStarted} />
+    <Link href="/tune">
+      <img src="/assets/get_started.svg" alt="getting started button" className={styles.getStarted} />
+    </Link>
   </div>
 );
 
---
+++ pages/tune.jsx
@@ -0,0 +1,9 @@
+import Nav from '../components/nav';
+
+const Dashboard = () => (
+  <>
+    <Nav />
+  </>
+);
+
+export default Dashboard;
