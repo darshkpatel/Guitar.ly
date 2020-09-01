316e5f09fae61dce807d231687e2ad4bce5d48bc
Darsh <darshkpatel@gmail.com>
2020-09-01 15:05:28 +0530

    navbar buttons

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,22 +1,37 @@
 import { signin, signout, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 import styles from './nav.module.css';
 
-
 const Nav = () => {
+  const router = useRouter();
   const [session, loading] = useSession();
 
   return (
     <nav className={styles.navBar}>
-      <noscript>
-        <style>{'.nojs-show { opacity: 1; top: 0; }'}</style>
-      </noscript>
-      <p
-        className={`nojs-show ${
-          !session && loading ? styles.loading : styles.loaded
-        }`}
+      <div
+        className={!session && loading ? styles.loading : styles.loaded}
       >
 
+        {/* Logo */}
         <img src="/assets/logo.svg" alt="logo" className={styles.logo} />
+
+        {/* Nav Buttons */}
+        <div className={styles.navItems}>
+          <Link href="/tune">
+            <span className={router.pathname === '/tune' ? styles.navButtonActive : styles.navButton}>Tune</span>
+          </Link>
+          |
+          <Link href="/learn">
+            <span className={router.pathname === '/learn' ? styles.navButtonActive : styles.navButton}>Learn</span>
+          </Link>
+          |
+          <Link href="/practice">
+            <span className={router.pathname === '/practice' ? styles.navButtonActive : styles.navButton}>Practice</span>
+          </Link>
+        </div>
+
+        {/* If not Logged In */}
         {!session && (
           <>
             <a
@@ -26,10 +41,12 @@ const Nav = () => {
                 signin();
               }}
             >
-              <span className={styles.navButton}>Login</span>
+              <span className={styles.loginButton}>Login</span>
             </a>
           </>
         )}
+
+        {/* If logged in */}
         {session && (
           <>
             <span
@@ -58,7 +75,7 @@ const Nav = () => {
             </span>
           </>
         )}
-      </p>
+      </div>
     </nav>
   );
 };
