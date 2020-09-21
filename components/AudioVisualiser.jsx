84b5b69c49a204a931c89636ecc2cf0c55952eab
Darsh <darshkpatel@gmail.com>
2020-09-21 20:40:09 +0530

    reduce datapoints

--- components/AudioVisualiser.jsx
+++ components/AudioVisualiser.jsx
@@ -3,13 +3,34 @@ import React, { useRef, useEffect } from "react";
 const AudioVisualiser = ({ audioData }) => {
   const canvas = React.createRef();
 
+  const filterData = (rawData) => {
+    const samples = 70; 
+    const blockSize = Math.floor(rawData.length / samples); 
+    const filteredData = [];
+    for (let i = 0; i < samples; i++) {
+      const blockStart = blockSize * i; 
+      let sum = 0;
+      for (let j = 0; j < blockSize; j++) {
+        sum += + Math.abs(rawData[blockStart + j]); 
+      }
+      filteredData.push(sum / blockSize); 
+    }
+    return filteredData;
+  };
+
+  const normalizeData = (filteredData) => {
+    const multiplier = Math.pow(Math.max(...filteredData), -1);
+    return filteredData.map((n) => n * multiplier);
+  };
+
   const draw = () => {
     const thisCanvas = canvas.current;
     const { height } = thisCanvas;
     const { width } = thisCanvas;
     const context = thisCanvas.getContext("2d");
     let x = 0;
-    const sliceWidth = (width * 1.0) / audioData.audioData.length;
+    const data = normalizeData(filterData(audioData.audioData));
+    const sliceWidth = (width * 1.0) / data.length;
 
     context.lineWidth = 2;
     context.strokeStyle = "#3999FF";
@@ -18,9 +39,9 @@ const AudioVisualiser = ({ audioData }) => {
     context.beginPath();
     context.moveTo(0, height / 2);
 
-    audioData.audioData.forEach((item) => {
-      const y = (item / 255.0) * height;
-      context.lineTo(x, y);
+    data.forEach((item) => {
+      const y = item * height;
+      context.lineTo(x, y / 2);
       x += sliceWidth;
     });
     context.lineTo(x, height / 2);
