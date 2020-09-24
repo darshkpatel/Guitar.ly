8a685d7bb950d605e383a904f0909f3321cae322
Darsh <darshkpatel@gmail.com>
2020-09-24 19:39:43 +0530

    learn APIs

---
+++ pages/api/learn/difficulty/[difficulty].js
@@ -0,0 +1,19 @@
+import connectDb from "../../../../utils/dbHelper";
+import Lesson from "../../../../models/Lesson";
+
+export default async function handler(req, res) {
+  const {
+    query: { difficulty },
+  } = req;
+
+  await connectDb();
+  
+  try {
+    const lesson = await Lesson.find({difficulty});
+    if (!lesson) res.status(404);
+    res.status(200).json({ success: true, data: lesson });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
+  
+}
---
+++ pages/api/learn/index.js
@@ -0,0 +1,12 @@
+import connectDb from "../../../utils/dbHelper";
+import Lesson from "../../../models/Lesson";
+
+export default async function handler(req, res) {
+  await connectDb();
+  try {
+    const lessons = await Lesson.find({});
+    res.status(200).json({ success: true, data: lessons });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
+}
