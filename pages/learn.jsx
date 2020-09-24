3c04adbfcbc7fc9c04df417325bdda3fb2577d2e
Darsh <darshkpatel@gmail.com>
2020-09-24 21:05:42 +0530

    login check for learn

--- pages/learn.jsx
+++ pages/learn.jsx
@@ -1,9 +1,20 @@
-import Layout from '../components/Layout';
+import { useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
 
-const Learn = () => (
-  <>
-    <Layout />
-  </>
-);
+const Learn = () => {
+  const router = useRouter();
+  const [session, loading] = useSession();
+
+  // Redirect to login page if not logged in 
+  if (!session && !loading) router.push("/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Flearn");
+
+  return (
+    <>
+      <Layout>
+      </Layout>
+    </>
+  );
+};
 
 export default Learn;
