bd7567ca5ede66c74e2b88c528ae54042cb78a86
Darsh <darshkpatel@gmail.com>
2020-09-24 19:11:40 +0530

    added practice schema

---
+++ models/Practice.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const PracticeSchema = new mongoose.Schema({
+  title: { type: String,  required: true },
+
+  difficulty: { type: String, enum: ["Easy", "Medium", "Hard"] },
+
+  slug: { type: String, required: true },
+
+  data: { type: Object }
+
+});
+
+export default mongoose.models.Practice || mongoose.model("Practice", PracticeSchema);
