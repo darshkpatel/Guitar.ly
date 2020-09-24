293c83976bfa55c8b051e63067327f001891a38e
Darsh <darshkpatel@gmail.com>
2020-09-24 19:02:46 +0530

    add lessons schema

---
+++ models/Lesson.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const LessonSchema = new mongoose.Schema({
+  title: { type: String,  required: true },
+
+  difficulty: { type: String, enum: ["Easy", "Medium", "Hard"] },
+
+  slug: { type: String, required: true },
+
+  tutorial: { type: String },
+
+  note: { type: String,  enum: ["A","B","C","D","E","F","G","A♯","B♯","C♯","D♯","E♯","F♯","G♯"] },
+
+  octave: { type: Number, min:1, max: 4},
+
+  data: { type: Object }
+
+});
+
+export default mongoose.models.Lesson || mongoose.model("Lesson", LessonSchema);
