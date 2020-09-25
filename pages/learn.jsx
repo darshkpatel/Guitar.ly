52a969f0e67e840af7366cfa28217eeffc00b9e6
Darsh <darshkpatel@gmail.com>
2020-09-25 18:01:28 +0530

    HotFix: Same page after signin

--- pages/learn.jsx
+++ pages/learn.jsx
@@ -1,14 +1,12 @@
-import { useSession } from 'next-auth/client';
-import { useRouter } from 'next/router';
+import { useSession, signin } from 'next-auth/client';
 import Layout from '../components/Layout';
 import Card from '../components/Card';
 
 const Learn = () => {
-  const router = useRouter();
   const [session, loading] = useSession();
 
   // Redirect to login page if not logged in
-  if (!session && !loading) router.push('/api/auth/signin?callbackUrl=/learn');
+  if (!session && !loading) signin(null, { callbackUrl: '/learn' });
 
   return (
     <>
