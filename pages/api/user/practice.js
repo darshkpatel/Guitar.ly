0763b6fbe9749f680a91306a8984583647b357e0
Darsh <darshkpatel@gmail.com>
2020-09-26 17:26:30 +0530

    add lesson and practice endpoints

---
+++ pages/api/user/practice.js
@@ -0,0 +1,32 @@
+import { getSession } from 'next-auth/client';
+import connectDb from '../../../utils/dbHelper';
+import Profile from '../../../models/Profile';
+
+export default async function handler(req, res) {
+  const { method } = req;
+  const session = await getSession({ req });
+  if (session) {
+    if (method === 'POST') {
+      const { practice } = req.body;
+      if (!practice) res.send(400);
+      await connectDb();
+
+      const userProfile = await Profile.findOneAndUpdate(
+        {
+          userEmail: session.user.email,
+        },
+        { $addToSet: { completedPractices: practice } },
+        { new: true, upsert: false }
+      );
+
+      if (!userProfile) return res.send(404);
+      res.send(userProfile);
+    } else {
+      res.send(405);
+    }
+  } else {
+    res.send(403);
+  }
+
+  return null;
+}
