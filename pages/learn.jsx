129c0e91f7ed19abb67ea7dbc13c6e30405bbb2d
Darsh <darshkpatel@gmail.com>
2020-09-26 17:26:20 +0530

    fetch profile serverside

--- pages/learn.jsx
+++ pages/learn.jsx
@@ -1,5 +1,4 @@
-import { useSession, signin } from 'next-auth/client';
-import useSWR from 'swr';
+import { useSession, getSession, signin } from 'next-auth/client';
 import Layout from '../components/Layout';
 import Card from '../components/Card';
 import CardTitle from '../components/CardTitle';
@@ -7,26 +6,18 @@ import ListRow from '../components/ListRow';
 import styles from '../components/styles/card.module.css';
 import connectDb from '../utils/dbHelper';
 import Lesson from '../models/Lesson';
+import Profile from '../models/Profile';
 
 const titleStyle = {
   fontSize: '26px',
   fontWeight: 400,
   margin: 10,
 };
-
-const fetcher = (email) =>
-  fetch('/api/profile', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email }),
-  }).then((res) => res.json());
-
-const Learn = ({ lessons }) => {
+const Learn = ({ lessons, profile }) => {
   const [session, loading] = useSession();
   // Redirect to login page if not logged in
   if (!session && !loading) signin(null, { callbackUrl: '/learn' });
 
-  const { profile, error } = useSWR(session?.user.email, fetcher);
   const getDifficultyStyle = (difficulty) => {
     switch (difficulty) {
       case 'Easy':
@@ -68,10 +59,10 @@ const Learn = ({ lessons }) => {
               </div>
               <div>
                 <span>
-                  {error && 'Error Fetching Data'}
                   {profile && profile.completedLessons.includes(lesson._id)
                     ? 'Completed'
                     : 'Incomplete'}
+                  {!profile && 'Error Fetching Data'}
                 </span>
               </div>
             </ListRow>
@@ -84,7 +75,7 @@ const Learn = ({ lessons }) => {
 
 export default Learn;
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
   // Fetch data server side for SSR
   await connectDb();
   const lessons = (await Lesson.find({})).map((document) => {
@@ -92,5 +83,11 @@ export async function getServerSideProps() {
     lesson._id = document._id.toString();
     return lesson;
   });
-  return { props: { lessons } };
+  const session = await getSession(context);
+
+  const profile = await Profile.findOne({
+    userEmail: session?.user.email,
+  }).lean();
+  profile._id = profile._id.toString();
+  return { props: { lessons, profile } };
 }
