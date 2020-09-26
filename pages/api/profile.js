92ec465a360aa8698a21891579c203f2dc90cfc8
Darsh <darshkpatel@gmail.com>
2020-09-26 16:47:25 +0530

    session validation for profile route

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -1,21 +1,26 @@
+import { getSession } from 'next-auth/client';
 import connectDb from '../../utils/dbHelper';
 import Profile from '../../models/Profile';
 
 export default async function handler(req, res) {
   const { method } = req;
+  const session = await getSession({ req });
+  if (session) {
+    if (method === 'POST') {
+      const { email } = req.body;
+      if (!email) res.send(400);
+      await connectDb();
 
-  await connectDb();
+      const userProfile = await Profile.findOne({ userEmail: email });
 
-  if (method === 'POST') {
-    const { email } = req.body;
-    if (!email) res.send(400);
-
-    const userProfile = await Profile.findOne({ userEmail: email });
-
-    if (!userProfile) return res.send(404);
-    res.send(userProfile);
+      if (!userProfile) return res.send(404);
+      res.send(userProfile);
+    } else {
+      res.send(405);
+    }
   } else {
-    res.send(405);
+    res.send(403);
   }
+
   return null;
 }
