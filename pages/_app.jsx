9bc6ecdb31ae588a3acede9ec7354d032f8d337c
Darsh <darshkpatel@gmail.com>
2020-09-26 17:28:52 +0530

    remove redux

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,14 @@
 import { Provider as AuthProvider } from 'next-auth/client';
-import { Provider } from 'react-redux';
-import { useStore } from '../redux/store';
 
 import '../styles.css';
 
 const App = ({ Component, pageProps }) => {
   const { session } = pageProps;
-  const store = useStore(pageProps.initialReduxState);
 
   return (
-    <Provider store={store}>
-      <AuthProvider options={{ site: process.env.SITE }} session={session}>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </Provider>
+    <AuthProvider options={{ site: process.env.SITE }} session={session}>
+      <Component {...pageProps} />
+    </AuthProvider>
   );
 };
 
--- redux/reducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import * as types from './types';
-
-// COMBINED REDUCERS
-const reducers = {};
-
-export default combineReducers(reducers);
