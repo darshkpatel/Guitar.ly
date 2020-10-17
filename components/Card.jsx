159e497cc1dd8a7a456bbc3e28a87a11d9f9e716
Darsh <darshkpatel@gmail.com>
2020-10-17 19:12:37 +0530

    Pass width from prop

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './styles/card.module.css';
 
-const Card = ({ children }) => {
-  return <div className={styles.card}>{children}</div>;
+const Card = ({ children, width }) => {
+  return (
+    <div className={styles.card} style={{ width: width || '80%' }}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
