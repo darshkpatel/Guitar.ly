3c6ba2063fa4faab40bb12f63946807242f714be
Darsh <darshkpatel@gmail.com>
2020-10-20 17:37:14 +0530

    Redux Setup

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,19 @@
 import { Provider as AuthProvider } from 'next-auth/client';
+import { Provider } from 'react-redux';
+import { useStore } from '../store';
 
 import '../styles.css';
 
 const App = ({ Component, pageProps }) => {
   const { session } = pageProps;
+  const store = useStore(pageProps.initialReduxState);
 
   return (
-    <AuthProvider options={{ site: process.env.SITE }} session={session}>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider options={{ site: process.env.SITE }} session={session}>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </Provider>
   );
 };
 
